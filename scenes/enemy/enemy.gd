85427a50b7660fe60a9179ab2bce1a9dfbc055bd
niqzart <qwert45hi@yandex.ru>
2025-01-08 23:34:30 +0300

    refactor: remove enemies from the tutorial

--- scenes/enemy/enemy.gd
+++
@@ -1,6 +0,0 @@
-extends RigidBody2D
-
-
-func _ready() -> void:
-    var mob_types: PackedStringArray = $AnimatedSprite2D.sprite_frames.get_animation_names()
-    $AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
